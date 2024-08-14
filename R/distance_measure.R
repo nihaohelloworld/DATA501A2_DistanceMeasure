57f90d740f1cfa25bde755fba43b70b6800fa071
nihaohelloworld <jieliu.nz@gmail.com>
2024-08-14 21:14:30 +1200

    version 2

--- R/distance_measure.R
+++ R/distance_measure.R
@@ -1,56 +1,145 @@
-#' Calculate Influence Measures
+#' Calculate Influence Measure and Plot
 #'
-#' This function calculates Cook's Distance, DFFITS, or Hadi's Influence Measure for a given linear model.
+#' This function calculates one of Cook's Distance, DFFITS, or Hadi's Influence Measure
+#' for a given linear model and then creates a scatter plot of the values.
 #'
 #' @param data A data frame containing the data.
 #' @param model An `lm` object representing the linear model.
 #' @param method A character string specifying the influence measure to calculate.
 #'        One of "cooks", "dffits", or "hadi".
-#' @return A vector of influence measures.
-#' @importFrom stats lm cooks.distance dfbetas hatvalues
+#' @return A scatter plot of the selected influence measure.
+#' @importFrom stats lm cooks.distance residuals
+#' @importFrom graphics plot abline
 #' @examples
 #' lm_model <- lm(mpg ~ wt + hp, data = mtcars)
-#' influence_measures(mtcars, lm_model, method = "cooks")
-#' influence_measures(mtcars, lm_model, method = "dffits")
-#' influence_measures(mtcars, lm_model, method = "hadi")
+#' influence_measure_plot(mtcars, lm_model, method = "cooks")
+#' influence_measure_plot(mtcars, lm_model, method = "dffits")
+#' influence_measure_plot(mtcars, lm_model, method = "hadi")
 #' @export
-influence_measures <- function(data, model, method = c("cooks", "dffits", "hadi")) {
+influence_measure_plot <- function(data, model, method = c("cooks", "dffits", "hadi")) {
   method <- match.arg(method)
 
   if (!inherits(model, "lm")) {
     stop("The model must be an object of class 'lm'.")
   }
 
-  if (method == "cooks") {
-    return(cooks.distance(model))
+  if (!is.data.frame(data)) {
+    stop("The data must be a data frame.")
   }
 
-  if (method == "dffits") {
-    # Calculate DFFITS
-    h <- hatvalues(model)
-    residuals <- residuals(model)
-    s <- summary(model)$sigma
-    dffits_values <- residuals / (s * sqrt(1 - h)) * sqrt(h / (1 - h))
-    return(dffits_values)
+  model_vars <- all.vars(formula(model))
+  if (!all(model_vars %in% names(data))) {
+    stop("The data does not contain all the variables used in the model.")
   }
 
-  if (method == "hadi") {
-    # Calculate Hadi's Influence Measure
-    X <- model.matrix(model)
-    h <- hatvalues(model)
-    residuals <- residuals(model)
-    s <- summary(model)$sigma
-    p <- ncol(X)
-
-    d <- residuals^2 / (1 - h)
-    Hadi_measure <- sqrt(p * h / (1 - h) + d / s^2)
-    return(Hadi_measure)
+  if (nrow(data) != nrow(model$model)) {
+    stop("The number of observations in the data does not match the number used in the model.")
   }
 
-  stop("Invalid method. Choose one of 'cooks', 'dffits', or 'hadi'.")
+  if (anyNA(data)) {
+    stop("The data contains NA values. Please remove or impute them before proceeding.")
+  }
+
+  if (any(is.infinite(as.matrix(data)))) {
+    stop("The data contains infinite values. Please remove or correct them before proceeding.")
+  }
+
+  if (ncol(data) < length(coef(model))) {
+    stop("The number of predictors in the data is less than the number of coefficients in the model.")
+  }
+
+  influence_values <- switch(
+    method,
+    "cooks" = cook_distance_custom(model),
+    "dffits" = dffits_custom(model),
+    "hadi" = hadi_influence_custom(model)
+  )
+
+  plot(
+    influence_values,
+    # type = "h",
+    main = paste("Influence Measure:", method),
+    xlab = "Observation",
+    ylab = paste(method, "value"),
+    col = "blue",
+    lwd = 2
+  )
+
+  return(influence_values)
 }
 
-data("mtcars")
-lm_model <- lm(mpg ~ wt + hp, data = mtcars)
-result <- influence_measures(mtcars, lm_model, method = "cooks")
-plot(result)
+#' Cook's Distance Calculation
+#'
+#' @inheritParams influence_measure_plot
+#' @return A vector of Cook's Distance values
+#' @export
+cook_distance_custom <- function(model) {
+  y_hat <- fitted(model)
+  n <- length(y_hat)
+  p <- length(coef(model))
+  residuals <- residuals(model)
+  sse <- sum(residuals^2)
+  sigma_sq_hat <- sse / (n - p - 1)
+  cooks_distances <- numeric(n)
+
+  for (i in 1:n) {
+    model_i <- lm(formula(model), data = model$model[-i, , drop = FALSE])
+    y_hat_i <- predict(model_i, newdata = model$model)
+    cooks_distances[i] <- sum((y_hat - y_hat_i)^2) / (sigma_sq_hat * (p + 1))
+  }
+
+  return(cooks_distances)
+}
+
+
+#' DFFITS Calculation
+#'
+#' @inheritParams influence_measure_plot
+#' @return A vector of DFFITS values
+#' @export
+dffits_custom <- function(model) {
+  y_hat <- fitted(model)
+  n <- length(y_hat)
+  p <- length(coef(model))
+  X <- model.matrix(model)
+  H <- X %*% solve(t(X) %*% X) %*% t(X)
+  dffits_values <- numeric(n)
+
+  for (i in 1:n) {
+    model_i <- lm(formula(model), data = model$model[-i, , drop = FALSE])
+    y_hat_i_minus <- predict(model_i, newdata = model$model[i, , drop = FALSE])
+    p_ii <- H[i, i]
+    residuals_i <- residuals(model_i)
+    sse_i <- sum(residuals_i^2)
+    sigma_i <- sqrt(sse_i / (n - p - 2))
+    dffits_values[i] <- (y_hat[i] - y_hat_i_minus) / (sigma_i * sqrt(p_ii))
+  }
+
+  return(dffits_values)
+}
+
+
+#' Hadi's Influence Measure Calculation
+#'
+#' @inheritParams influence_measure_plot
+#' @return A vector of Hadi's Influence Measure values
+#' @export
+hadi_influence_custom <- function(model) {
+  n <- length(model$fitted.values)
+  p <- length(coef(model))
+  X <- model.matrix(model)
+  H <- X %*% solve(t(X) %*% X) %*% t(X)
+  residuals <- residuals(model)
+  sse <- sum(residuals^2)
+  hadi_values <- numeric(n)
+
+  for (i in 1:n) {
+    p_ii <- H[i, i]
+    d_i <- residuals[i] / sqrt(sse)
+    hadi_values[i] <- (p_ii / (1 - p_ii)) + ((p + 1) / (1 - p_ii)) * (d_i^2 / (1 - d_i^2))
+  }
+
+  return(hadi_values)
+}
+
+
